5f02dd780b7977dd4b26c7111d2caf699ce196db
Guilherme França <souzagfranca@gmail.com>
2023-09-21 21:54:56 -0300

    Pratica 07 Navegação

---
+++ Praticas/pratica-07/App.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+import NavegacaoPilha from './routes/NavegacaoPilha';
+import NavegacaoGaveta from './routes/NavegacaoGaveta';
+import NavegacaoAba from './routes/NavegacaoAba';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <NavegacaoPilha />
+    </NavigationContainer>
+  );
+};
+
+export default App;
